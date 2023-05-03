92b3f0cc1d5ae1ebee0efcd2678754b19058e8da
SyabAhmad <syabblogger@gmail.com>
2023-05-03 21:59:34 +0500

    Initial commit

---
+++ app/src/main/java/com/example/gpacalculatorredesign/calculateCGPA.java
@@ -0,0 +1,14 @@
+package com.example.gpacalculatorredesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class calculateCGPA extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculate_cgpa);
+    }
+}
